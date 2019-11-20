13e40fc9ec17adb2e02d6843608f95fd361489a7
perrymacdougall <perrymacdougall@gmail.com>
2019-11-20 17:17:56 -0500

    installed Axios and configured it to post form data into db

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Create extends Component {
     constructor(props) {
@@ -24,7 +25,13 @@ export default class Create extends Component {
 
     onSubmit = (e) => {
         e.preventDefault();
-        console.log(`name is ${this.state.name} and port is ${this.state.port}`);
+        const serverport = {
+            name: this.state.name,
+            port: this.state.port
+        }
+        axios.post('http://localhost:4000/serverport/add', serverport)
+        .then(res => console.log(res.data));
+        
         this.setState({
             name: '',
             port: ''
