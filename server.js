ba8ba4506f7b929210e78630cb5b371237c4c56a
perrymacdougall <perrymacdougall@gmail.com>
2019-11-20 16:19:27 -0500

    connected db to server

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const PORT = 4000;
 const cors = require('cors');
+const config = require('./db');
 
+mongoose.connect(config.DB).then(
+    () => { console.log('Database is connected') },
+    err => { console.log('Can not connect to the database' +err)
+});
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
