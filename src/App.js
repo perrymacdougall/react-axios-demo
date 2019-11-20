121d63ff7351183be8f53b4c0092aa74e50ec197
perrymacdougall <perrymacdougall@gmail.com>
2019-11-20 13:07:03 -0500

    installed react-router-dom and configured routes for the Create and Index components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Create from './components/Create';
+import Index from './components/Index';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
-      <div class="container">
-        React Axios Demo
-      </div>
+      <Router>
+        <div className="container">
+          <h2>Welcome to my React Axios Demo</h2>
+          <ul>
+            <li><Link to={'/create'}>Create</Link></li>
+            <li><Link to={'/index'}>List</Link></li>
+          </ul>
+          <hr />
+          <Switch>
+            <Route exact path='/create' component={Create} />
+            <Route exact path='/index' component={Index} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
